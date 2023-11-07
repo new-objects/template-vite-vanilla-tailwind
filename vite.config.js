273a531cd9ff372b131b909dbf4317cc600557db
mi-skam <maksim@miskam.xyz>
2023-11-07 12:36:20 +0100

    adding gh-pages deploy option
    - adding a "deploy" target
    - adding gh-pages package
    - vite: setting base option to [comply](https://vitejs.dev/guide/static-deploy.html#github-pages) with github pag
    es setting

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig({
+  base:
+    process.env.NODE_ENV === 'production' ? '/template-frontend/' : '/',
   plugins: [
     eslintPlugin({
       cache: false,
