5c7105dd422e7ff2b253a6f445040080ab3ff515
mi-skam <maksim@miskam.xyz>
2023-11-20 12:04:46 +0100

    adding qrcode for dev server

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import eslintPlugin from 'vite-plugin-eslint';
+import { qrcode } from 'vite-plugin-qrcode';
 
 export default defineConfig({
   base: process.env.DEPLOY_ENV === 'gh-pages' ? '/template-frontend/' : '/',
   plugins: [
+    qrcode(),
     eslintPlugin({
       cache: false,
-      failOnError: false
+      failOnError: false,
     }),
   ],
 });
